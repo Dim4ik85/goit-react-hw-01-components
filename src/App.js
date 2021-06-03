7d33292e8f848dbcf540e269fd4f9d62b6e32941
Dmytro Sagaidachnyi <lafuma4me@gmail.com>
2021-06-03 12:35:33 +0300

    active

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Profile from "./components/Profile/Profile";
 import user from "./baza/user.json" ;
+import statisticalData from "./baza/statistical-data.json";
+import Statistics from "./components/Statistics/Statistics";
 
 const App = () => {
   return (
@@ -12,7 +14,14 @@ const App = () => {
           avatar={user.avatar}
           stats={user.stats}
         />
+
+        <Statistics title="Upload stats" stats={statisticalData} />;
+        {/* <Statistics stats={statisticalData} />; */}
+    
+    
     </div>
+    
+    
   );
 };
  export default App;
